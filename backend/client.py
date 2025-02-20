2d0c3b1282a6604c50ecfd388cdaa8a330067e68
Luqman Headley <luqmanforge@gmail.com>
2025-02-20 03:55:55 -0500

    refactor: client CLI now uses inventoryAPI

--- backend/client.py
+++ backend/client.py
@@ -1,71 +1,14 @@
 import argparse
-import json
 import os
 
-import requests
 from dotenv import load_dotenv
-from tabulate import tabulate
+
+from api.inventoryAPI import get_inventory, get_logs, restock_item, checkout_item, delete_all_items, get_item, \
+    create_item
 
 load_dotenv()
 
 BASE_URL = os.getenv('INVENTORY_API_URL', 'http://127.0.0.1:8001')
-
-
-def print_table(response):
-    try:
-        data = response.json()
-        print(data)
-        if response.status_code != 200 and response.status_code != 201:
-            print(f'Error: Received status code {response.status_code}. Reason is {data["detail"]}')
-
-        if isinstance(data, list) and data:
-            print(tabulate(data, headers='keys', tablefmt='grid'))
-        elif isinstance(data, dict) and 'message' in data:
-            print(f'Response:  {data["message"]}')
-        else:
-            print(json.dumps(data, indent=4))
-    except json.JSONDecodeError:
-        print(f'Error: Unable to parse json response. Raw response is as follows: {response.text}')
-    except KeyError:
-        print(f'Error: Unable to parse response. Raw response is as follows: {response.text}')
-
-
-def get_inventory():
-    response = requests.get(f'{BASE_URL}/items')
-    print_table(response)
-
-
-def get_item(name):
-    response = requests.get(f'{BASE_URL}/item', json={'name': name})
-    print_table(response)
-
-
-def get_logs():
-    response = requests.get(f'{BASE_URL}/logs')
-    print_table(response)
-
-
-def restock_item(name, quantity):
-    response = requests.post(f'{BASE_URL}/restock', json={'name': name, 'quantity': quantity})
-    print_table(response)
-
-
-def checkout_item(name, quantity):
-    response = requests.post(f'{BASE_URL}/checkout', json={'name': name, 'quantity': quantity})
-    print_table(response)
-
-
-def delete_all_items():
-    response = requests.delete(f'{BASE_URL}/delete_all')
-    print_table(response)
-
-
-def create_item(name, initial_stock, unit_weight, price):
-    response = requests.post(f'{BASE_URL}/create',
-                             json={'name': name, 'initial_stock': initial_stock, 'unit_weight': unit_weight,
-                                   'price': price})
-    print_table(response)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Client for Inventory System')
@@ -83,28 +26,28 @@ if __name__ == '__main__':
         BASE_URL = 'http://127.0.0.1:8001'
 
     if args.action == 'inventory':
-        get_inventory()
+        print(get_inventory().formatted_string())
     elif args.action == 'logs':
-        get_logs()
+        print(get_logs().formatted_string())
     elif args.action == 'restock':
         if not args.name or args.quantity is None:
             print('Error: --name and --quantity are required for restock.')
         else:
-            restock_item(args.name, args.quantity)
+            print(restock_item(args.name, args.quantity).formatted_string())
     elif args.action == 'checkout':
         if not args.name or args.quantity is None:
             print('Error: --name and --quantity are required for checkout.')
         else:
-            checkout_item(args.name, args.quantity)
+            print(checkout_item(args.name, args.quantity).formatted_string())
     elif args.action == 'delete_all':
-        delete_all_items()
+        print(delete_all_items().formatted_string())
     elif args.action == 'check':
         if not args.name:
             print('Error: --name is required.')
         else:
-            get_item(args.name)
+            print(get_item(args.name).formatted_string())
     elif args.action == 'create':
         if not args.name or args.quantity is None or args.unit_weight is None or args.price is None:
             print('Error: --name, --quantity, --unit_weight and --price are required for creation.')
         else:
-            create_item(args.name, args.quantity, args.unit_weight, args.price)
+            print(create_item(args.name, args.quantity, args.unit_weight, args.price).formatted_string())
