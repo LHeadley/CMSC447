e46014a3a34b8938f079b26c25edf1ec48b5c113
Benjamin Whitehurst <b236@umbc.edu>
2025-03-21 17:16:11 -0400

    Added export inventory to csv file functionality.

--- form_io/form_io.py
+++ form_io/form_io.py
@@ -8,20 +8,41 @@ from models.response_schemas import ItemResponse, TransactionResponse, MessageRe
 from server import app, get_items, db_context
 
 
-def form_io_read_file(filename):
+# - File input/output -
+
+# file format: first col should be product name, last col quantity (other cols ignored)
+# TODO: throw errors
+# TODO: detect headers
+# TODO: excel .xlsx files
+def read_file(filename):
     with open(filename, 'r', newline='') as csvfile:
         # detect dialect with sample from csvfile
         dialect = csv.Sniffer().sniff(csvfile.read(1024))
         csvfile.seek(0)
         # grab rows from file
-        myreader = csv.reader(csvfile, dialect)
+        freader = csv.reader(csvfile, dialect)
         data = []
-        for row in myreader:
+        for row in freader:
             data.append([row[0], int(row[-1])])
 
     return data
 
-def form_io_import(url, filename):
+# data format: list of 2-len ['name', quantity] lists 
+def write_file(filename, data):
+    
+    with open(filename, 'w', newline='') as csvfile:
+        fwriter  = csv.writer(csvfile, delimiter=',', dialect='excel');
+
+        for row in data:
+            fwriter.writerow(row);
+                
+    return data;
+
+
+# - Database interaction -
+
+# TODO: throw errors
+def db_import(url, filename):
     # get relevant data from file
     data = form_io_read_file(filename)
     
@@ -59,7 +80,39 @@ def form_io_import(url, filename):
             elif res.status_code == ResponseStatus.UNKNOWN_ERROR:
                 print('An unknown error occurred.')
 
-            # You can also get a human-readable version of the error with:
             print(res.formatted_string())
-            # or use res.error. Both will be equal if the request failed
+
+
+# TODO: throw errors
+def db_export(url, filename):
+
+    # send request to db
+    res: APIResponse = inventoryapi.get_inventory(url=url, timeout=50)
+
+    # below is plagiarized from api_example.py
+        
+    # Check if it was successful
+    if res.is_success:
+        # Since it was successful, the response type is guaranteed to be a MessageResponse
+        message: MessageResponse = res.model
+        print(message)
+    else:
+        # If it wasn't successful, res.model will be set to None, but you can check the response status code like this:
+        if res.status_code == ResponseStatus.CONFLICT:
+            print('HTTP 409 Conflict')
+        elif res.status_code == ResponseStatus.INTERNAL:
+            print('HTTP 500 Internal Server Error')
+        elif res.status_code == ResponseStatus.CONNECTION_ERROR:
+            print('Could not connect to server.')
+        elif res.status_code == ResponseStatus.TIMEOUT:
+            print('Server took too long to respond.')
+        elif res.status_code == ResponseStatus.UNKNOWN_ERROR:
+            print('An unknown error occurred.')
+            
+            print(res.formatted_string())
+
+    items: list[ItemResponse] = res.model
+    data = [[x.name, x.stock] for x in items]
+
+    write_file(filename, data)
     
--- test_form_io.py
+++ test_form_io.py
@@ -4,8 +4,13 @@ from form_io import form_io
 TEST_URL = 'http://82.29.198.185:8002'
 # name of the csv to read (can detect delimeters)
 TEST_FILENAME = 'form_io/example.csv'
+TEST_EXPORTNAME = 'form_io/export.csv'
 
 print("^ form_io functions loaded ^")
-print(form_io.form_io_import(TEST_URL, TEST_FILENAME))
+#print(form_io.db_import(TEST_URL, TEST_FILENAME))
+
+#print( form_io.write_file(TEST_EXPORTNAME, form_io.read_file(TEST_FILENAME)) )
+
+form_io.db_export(url=TEST_URL, filename=TEST_EXPORTNAME)
 
 # launch 
