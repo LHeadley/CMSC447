44a180aebb334f303c5e5a07556f69a909a1fc14
nenadz1 <nenadz1@umbc.edu>
2025-03-25 20:51:07 -0400

    capitalized Admin_cart class name

--- frontend_app/admin_cart.py
+++ frontend_app/admin_cart.py
@@ -1,6 +1,6 @@
 import cart
 
-class admin_cart(cart):
+class Admin_cart(cart):
     def __init__(self, cart_owner=None):
         super.__init__(self, cart_owner)
         self.restock_button = None
