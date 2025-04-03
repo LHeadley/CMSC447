d6c792e844c7f02544de0bc36b8ae86932b216b8
pfuller11 <pfuller1@umbc.edu>
2025-04-03 16:43:38 -0400

    removed manual ID input, added create-item functionality (including popups)

--- frontend_app/screens/admin.py
+++ frontend_app/screens/admin.py
@@ -1,7 +1,15 @@
+import json
+
 from nicegui import APIRouter, ui
+from starlette.responses import JSONResponse
+from fastapi import Response
 
 from frontend_app.analytics import AnalyticsRequest
 from frontend_app.common import show_inventory, show_cart
+
+from server import db_context, create_item
+from models.request_schemas import CreateRequest
+from models.response_schemas import MessageResponse
 
 router = APIRouter(prefix='/admin')
 
@@ -11,6 +19,7 @@ router = APIRouter(prefix='/admin')
 def admin_page():
     ui.page_title('Admin | Retriever Essentials')
     ui.label('Admin Dashboard')
+    ui.colors(primary='#EBB000') # styling to umbc gold
     show_inventory()
 
     show_cart('admin', True)
@@ -51,29 +60,24 @@ def admin_page():
         make_btn = ui.button("CREATE NEW ITEM")
 
         # item information
-        make_id = ui.number("Item ID", value=0)
         make_name = ui.input("Item Name", value="")
         make_amt = ui.number("Amount In-Stock", value=0)
         make_max = ui.number("Max Allowed per Checkout", value=0)
 
         # set create-item button to take info from input fields
-        make_btn.on_click(lambda: make_item(make_id.value, make_name.value, make_amt.value, make_max.value))
+        make_btn.on_click(lambda: make_item(make_name.value, make_amt.value, make_max.value))
         # bind create-item button clickability to valid input; have to bind to all
-        make_btn.bind_enabled_from(make_id, "value",
-                                   lambda v: valid_input(v, make_name.value, make_amt.value, make_max.value))
         make_btn.bind_enabled_from(make_name, "value",
-                                   lambda v: valid_input(make_id.value, v, make_amt.value, make_max.value))
+                                   lambda v: valid_input(v, make_amt.value, make_max.value))
         make_btn.bind_enabled_from(make_amt, "value",
-                                   lambda v: valid_input(make_id.value, make_name.value, v, make_max.value))
+                                   lambda v: valid_input(make_name.value, v, make_max.value))
         make_btn.bind_enabled_from(make_max, "value",
-                                   lambda v: valid_input(make_id.value, make_name.value, make_amt.value, v))
+                                   lambda v: valid_input(make_name.value, make_amt.value, v))
 
 
-def valid_input(id: int, name: str, amt: int, max: int) -> bool:
+def valid_input(name: str, amt: int, max: int) -> bool:
     # to check all potential input values for validity whenever one is changed
-    # TODO: check database for repeat ID, name
-    if id < 0:
-        return False
+    # TODO: validation directly in inputs? (next sprint?)
 
     if len(name.strip()) <= 0:
         return False
@@ -87,9 +91,28 @@ def valid_input(id: int, name: str, amt: int, max: int) -> bool:
     return True
 
 
-def make_item(id: int, name: str, amt: int, max: int):
-    # dummy function for making new cart item
-    ui.notify(f"ITEM MADE: id:{id}, name:{name}, amt:{amt}, max:{max}", close_button="close")
+def make_item(name: str, amt: int, max: int):
+    # add new item to the database
+    with db_context() as db:
+        # attempt to add item to database
+        result = create_item(CreateRequest(name=name, initial_stock=amt, max_checkout=max),
+                    Response(), db=db)
+
+        # display popup for success or failure
+        if isinstance(result, MessageResponse):
+            # success
+            with ui.dialog() as dialog, ui.card():
+                ui.label(result.message)
+                ui.button("Close", on_click=dialog.close)
+            dialog.open()
+        elif isinstance(result, JSONResponse):
+            # failure (item already exists)
+            with ui.dialog() as dialog, ui.card():
+                ui.label(f"Error {result.status_code}: {json.loads(result.body.decode())["message"]}")
+                ui.button("Close", on_click=dialog.close)
+            dialog.open()
+
+        #TODO: reload database/screen/?
 
 
 # dummy functions for import/export #
