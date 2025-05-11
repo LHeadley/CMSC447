8a6df9f47466ae3eb90e186881db20971acde669
Luqman Headley <luqmanforge@gmail.com>
2025-05-11 11:38:01 -0400

    fix: Image names are always capitalized on creation

--- frontend_app/screens/admin.py
+++ frontend_app/screens/admin.py
@@ -165,7 +165,7 @@ def make_item(name_field: ui.input,
               max_field: ui.number,
               upload_field: ui.upload,
               img_data: dict):
-    name = name_field.value
+    name = name_field.value.stip().upper()
     amt = amt_field.value
     max_val = max_field.value
     print(img_data)
@@ -173,7 +173,7 @@ def make_item(name_field: ui.input,
     # add new item to the database
     with db_context() as db:
         # attempt to add item to database
-        form_name = name.strip().upper()
+        form_name = name
         result = create_item(CreateRequest(name=form_name, initial_stock=amt, max_checkout=max_val),
                              Response(), db=db)
 
